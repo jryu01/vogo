b6fc469ff2bc21d6b8092ed0a1877ddf53921547
Jaehwan Ryu <time0121@gmail.com>
2015-02-14 15:34:05 -0500

    update app config

--- www/app/app.js
+++ www/app/app.js
@@ -15,22 +15,6 @@ angular.module('voteit', [
   'voteit.home',
   'voteit.profile',
 ])
-.run(function ($ionicPlatform) {
-// .run(function ($ionicPlatform, auth) {
-//   auth.authenticate({id: '54caf3a20f16cd696750c96c', name: 'Jaehwan Ryu'}, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1NGNhZjNhMjBmMTZjZDY5Njc1MGM5NmMiLCJleHAiOjE0MjgyODI0MzA4Njd9.FHIJ-Hb9msWPQ7aoJ64HNjOwMpfX2S-S6KeOPw5toEA');
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      window.StatusBar.styleDefault();
-    }
-  });
-
-})
 
 .config(function ($stateProvider, $urlRouterProvider, RestangularProvider, config) {
 
@@ -53,4 +37,26 @@ angular.module('voteit', [
   $urlRouterProvider.otherwise('/tab/home');
 
   RestangularProvider.setBaseUrl(config.baseUrl);
+})
+
+.run(function ($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      window.StatusBar.styleDefault();
+    }
+  });
+
+})
+
+.run(function (config, auth) {
+  // login with predefined user on development
+  if (config.env === 'developmetn') {
+    auth.authenticate({id: '54caf3a20f16cd696750c96c', name: 'Jaehwan Ryu'}, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1NGNhZjNhMjBmMTZjZDY5Njc1MGM5NmMiLCJleHAiOjE0MjgyODI0MzA4Njd9.FHIJ-Hb9msWPQ7aoJ64HNjOwMpfX2S-S6KeOPw5toEA');
+  }
 });
