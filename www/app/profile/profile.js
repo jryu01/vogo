a69ba0c57f090cffb48937db6633729f0ed4eb7c
Jaehwan Ryu <time0121@gmail.com>
2015-03-03 14:29:05 -0500

    change profile subtabs

--- www/app/profile/profile.js
+++ www/app/profile/profile.js
@@ -22,10 +22,19 @@ angular.module('voteit.profile', [
 .controller('ProfileCtrl', [
   'auth',
   '$scope',
-function (auth, $scope) {
+  '$ionicTabsDelegate',
+function (auth, $scope, $ionicTabsDelegate) {
   var self = this;
 
   self.profileName = auth.getUser().name;
+
+  self.tabs = {
+    selected: 0
+  };
+  self.selectTab = function (index) {
+    $ionicTabsDelegate.$getByHandle('custom-tabs-handle').select(index);
+    self.tabs.selected = index;
+  };
 
   $scope.$on('$ionicView.beforeEnter', function () {
     if (ionic.Platform.isAndroid()) {
