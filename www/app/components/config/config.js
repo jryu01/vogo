aead92f215ec1ed83c7e4803d0a5fd5f0bb62fce
Jaehwan Ryu <time0121@gmail.com>
2015-03-10 23:35:59 -0400

    update version

--- www/app/components/config/config.js
+++ www/app/components/config/config.js
@@ -4,7 +4,7 @@
 angular.module('voteit.config', [])
 
 .constant('config', {
-  "version": "0.0.2",
+  "version": "0.0.3",
   "env": "development",
   "baseUrl": "http://localhost:3000/api",
   "fbAppId": "794909417212702"
