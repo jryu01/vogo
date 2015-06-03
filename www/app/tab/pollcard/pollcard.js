c6ca0e2f5158cfb20304a315b9a4e58b76efbdb8
Jaehwan Ryu <time0121@gmail.com>
2015-06-03 00:31:38 -0400

    change profile style

--- www/app/tab/pollcard/pollcard.js
+++ www/app/tab/pollcard/pollcard.js
@@ -28,18 +28,7 @@ angular.module('voteit.tab.pollcard', [])
 .controller('PollcardCtrl', [
   '$scope',
   '$stateParams', 
-  'Polls',
-function ($scope, $stateParams, Polls) {
+function ($scope, $stateParams) {
   var self = this;
-  // self.poll = $stateParams.poll;
   $scope.poll = $stateParams.poll;
-  
- // self.vote = function (poll, answerNum) {
-    // Polls.vote(poll, answerNum);
-    // if (!poll.isVotedByMe) {
-    //   poll['answer' + answerNum].numVotes += 1;
-    //   poll.isVotedByMe = true;
-    //   poll.answerVotedByMe = answerNum;
-    // }
-  // };
 }]);
