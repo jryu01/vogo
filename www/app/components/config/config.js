1f173d02a201495a02d3461f3228c16741ed430b
Jaehwan Ryu <time0121@gmail.com>
2015-06-16 17:20:11 -0400

    update poll card to go to detail when title is clicked

--- www/app/components/config/config.js
+++ www/app/components/config/config.js
@@ -5,7 +5,7 @@ angular.module('voteit.config', [])
 
 .constant('config', {
   "version": "0.0.3",
-  "env": "development",
-  "baseUrl": "http://localhost:3000/api",
-  "fbAppId": "794909417212702"
+  "env": "test",
+  "baseUrl": "https://voteit-test.herokuapp.com/api",
+  "fbAppId": "794909633879347"
 });
