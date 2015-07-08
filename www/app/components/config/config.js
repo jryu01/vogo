e7aa8df37ef71e226220e978d64b1dc8b34f0de1
Jaehwan Ryu <time0121@gmail.com>
2015-07-08 10:59:15 -0400

    change notifiation style

--- www/app/components/config/config.js
+++ www/app/components/config/config.js
@@ -4,7 +4,7 @@
 angular.module('voteit.config', [])
 
 .constant('config', {
-  "version": "0.0.3",
+  "version": "0.2.2",
   "env": "development",
   "baseUrl": "http://localhost:3000/api",
   "fbAppId": "794909417212702"
